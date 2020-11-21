a86cbfca0c573b19a97fc6803163c51bb9959d69
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-21 12:50:17 -0500

    constructor

---
+++ src/com/fabrizio/Email.java
@@ -0,0 +1,22 @@
+package com.fabrizio;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+//    todo: constructor to receive first and last name
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName=lastName;
+    }
+
+//    todo: ask for the department
+
+//            todo: generate a random password
+
+//        todo: set the mailbox capacity and alternate email and change the password
+}
