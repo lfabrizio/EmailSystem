7adc289042f11b2679861a54c967fec29ca46a23
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-23 18:34:19 -0500

    email system

--- src/com/fabrizio/Email.java
+++ src/com/fabrizio/Email.java
@@ -7,8 +7,11 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
-    private int mailboxCapacity;
+    private String email;
+    private int mailboxCapacity = 500;
+    private int defaultPasswordLength = 10;
     private String alternateEmail;
+    private String companySuffix = "company.com";
 
 
     public Email(String firstName, String lastName){
@@ -17,6 +20,11 @@ public class Email {
         System.out.println("Email created for " + this.lastName + " " + this.firstName);
         this.department = setDepartment();
         System.out.println("Department is " + this.department);
+        this.password= randomPassword(defaultPasswordLength);
+        System.out.println("your password is " + this.password);
+        // combine elements
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+        System.out.println("Your email is " + email);
     }
 
 //    todo: ask for the department
@@ -40,11 +48,41 @@ public class Email {
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!";
         char[] password = new char[length];
         for(int i = 0; i < length;i++){
-           int rand = (int)Math.random() * passwordSet.length();
+           int rand = (int) (Math.random() * passwordSet.length());
            password[i] = passwordSet.charAt(rand);
         }
         return new String(password);
     }
 
 //        todo: set the mailbox capacity and alternate email and change the password
+    public void setMailboxCapacity(int capacity){
+        this.mailboxCapacity = capacity;
+    }
+
+    public void setAlternateEmail(String altEmail){
+        this.alternateEmail = altEmail;
+    }
+
+    //change password
+    public void changePassword(){
+        this.password = password;
+    }
+
+    public int getMailboxCapacity(){
+        return mailboxCapacity;
+    }
+
+    public String getAlternateEmail(){
+        return alternateEmail;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+   //no
+     public String showInfo(){
+        return "Display NAme: " + firstName + " " + lastName + "\n Company Name " + email + "\n Mailbox Capacity " + mailboxCapacity;
+     }
+
 }
--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Email one = new Email("Lou", "Fab");
+        System.out.println(one.showInfo());
     }
 }
